50454e1afec82c7038611d55a4634c4cfecce1d6
Diego <davirguezc18@gmail.com>
2022-06-23 16:34:33 -0500

    Consume de servicio GET para buscar los paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -13,6 +14,7 @@ import { AppRountingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRountingModule, //importar aca el modulo de las rutas
+    HttpClientModule, //modulo que se basa en rxjs
     PaisModule,
     SharedModule,
   ],
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,9 +9,13 @@ export class PorPaisComponent {
 
  termino:string = '';
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   buscar(){
+    this.paisService.buscarPais(this.termino)
+    .subscribe( resp =>{         //debo colocar subscribe para que se dispare la peticion
+      console.log(resp);
+    } );
     console.log(this.termino);
   }
 
