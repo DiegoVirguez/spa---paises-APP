94bf4215fcb321ccb71fdc9d5dfe483185a9a830
Diego <davirguezc18@gmail.com>
2022-06-24 11:57:05 -0500

    Generar el input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,6 +10,7 @@ export class PorPaisComponent {
 
  termino:string = '';
  hayError: boolean = false;
+ paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
@@ -17,8 +19,14 @@ export class PorPaisComponent {
     
     this.paisService.buscarPais(this.termino)
     .subscribe({
-      next: (paises) => console.log('DATA::',paises),
-      error: (_) => this.hayError = true
+      next: (paises) => {
+        console.log('DATA::',paises)
+        this.paises = paises;
+      },
+      error: (_) => {
+        this.hayError = true;
+        this.paises = [];
+      }
     });
   }
 
