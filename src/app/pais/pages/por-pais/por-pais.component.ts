2ea879e78db8ecd400bb723c2b85ad3db93eeb7b
Diego <davirguezc18@gmail.com>
2022-06-24 17:10:01 -0500

    generacion del Output

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -14,8 +14,10 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService) { }
 
-  buscar(){
+  buscar( terminoEvento: string){
     this.hayError = false;
+    this.termino = terminoEvento;
+    console.log(this.termino);
     
     this.paisService.buscarPais(this.termino)
     .subscribe({
